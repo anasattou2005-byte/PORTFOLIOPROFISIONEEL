f29feccbac94f6541e969ef2f3b0f576964387ba
anasattoudiv <attouanas10@gmail.com>
2026-04-29 00:43:25 +0100

    update

---
+++ front-end/script.js
@@ -0,0 +1,7 @@
+
+  window.addEventListener("load", function() {
+    setTimeout(() => {
+      document.getElementById("preloader").style.display = "none";
+      document.getElementById("content").style.display = "block";
+    }, 1200); // الوقت ديال التحميل
+  });
