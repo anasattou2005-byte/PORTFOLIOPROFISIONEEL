3f3da424a9c4d63a01e0df74cace3a7e41729f2a
anasattoudiv <attouanas10@gmail.com>
2026-04-29 01:50:39 +0100

    update

--- front-end/script.js
+++ front-end/script.js
@@ -5,3 +5,13 @@
       document.getElementById("content").style.display = "block";
     }, 1200); // الوقت ديال التحميل
   });
+
+
+    window.onload = () => {
+    document.querySelector(".php").style.width = "85%";
+    document.querySelector(".js").style.width = "75%";
+    document.querySelector(".html").style.width = "75%";
+    document.querySelector(".react").style.width = "75%";
+    document.querySelector(".laravel").style.width = "50%";
+    document.querySelector(".wordpress").style.width = "50%";
+  };
